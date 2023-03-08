f73cfed5ea1b9844ba631034cbea3658cab2825a
Nedyalko Velkov <NVelkov@nemetschek.bg>
2023-03-08 15:22:33 +0200

    Add new word add-in with a button for bold toggle on selected text

---
+++ WordDynamicControls/WordDynamicControls/MyRibbon.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WordDynamicControls
+{
+    public partial class MyRibbon
+    {
+
+        private void MyRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            Globals.ThisAddIn.ToggleTextBoldOnText();
+        }
+    }
+}
---
+++ WordDynamicControls/WordDynamicControls/ThisAddIn.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+using System.Windows.Forms;
+
+namespace WordDynamicControls
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        internal void ToggleTextBoldOnText()
+        {
+            Word.Selection selection = this.Application.Selection;
+
+            if (selection != null && selection.Range != null)
+            {
+                selection.Font.Bold = selection.Font.Bold == 0 ? 1:0;
+            }
+        }
+
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
