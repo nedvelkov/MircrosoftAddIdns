8b2e52874a0fcd67af1f60c0faa68df236113daa
Nedyalko Velkov <NVelkov@nemetschek.bg>
2023-03-09 17:31:54 +0200

    Add functionality to the CreateTable method in ThisAddIn class

--- ExcelAddInWithButtons/ExcelAddInWithButtons/ThisAddIn.cs
+++ ExcelAddInWithButtons/ExcelAddInWithButtons/ThisAddIn.cs
@@ -6,6 +6,7 @@ using System.Xml.Linq;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Office.Tools.Excel;
+using System.Data;
 
 namespace ExcelAddInWithButtons
 {
@@ -27,7 +28,37 @@ namespace ExcelAddInWithButtons
 
         internal void CreateTable()
         {
+            int[] Ages = { 32, 44, 28, 61 };
+            string[] Names = { "Reggie", "Sally", "Henry", "Christine" };
 
+            // Create a data table with two columns.
+            DataSet ds = new DataSet();
+            DataTable table = ds.Tables.Add("Customers");
+            DataColumn column1 = new DataColumn("Names", typeof(string));
+            DataColumn column2 = new DataColumn("Ages", typeof(int));
+            table.Columns.Add(column1);
+            table.Columns.Add(column2);
+
+            // Add the four rows of data to the table.
+            DataRow row;
+            for (int i = 0; i < 4; i++)
+            {
+                row = table.NewRow();
+                row["Names"] = Names[i];
+                row["Ages"] = Ages[i];
+                table.Rows.Add(row);
+            }
+
+            Worksheet worksheet = Globals.Factory.GetVstoObject(
+            Globals.ThisAddIn.Application.ActiveWorkbook.Worksheets[1]);
+
+            Microsoft.Office.Tools.Excel.ListObject list1;
+            Excel.Range cell = worksheet.Range["$A$1:$D$4"];
+            list1 = worksheet.Controls.AddListObject(cell, "list1");
+
+
+            // Bind the list object to the table.
+            list1.SetDataBinding(ds, "Customers");
         }
 
         #region VSTO generated code
